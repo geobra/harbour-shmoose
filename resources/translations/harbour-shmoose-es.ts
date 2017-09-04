444da78aeba0ff54116f43da4215f211c95df824
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-09-04 19:43:53 -0400

    Update harbour-shmoose-es.ts

--- resources/translations/harbour-shmoose-es.ts
+++ resources/translations/harbour-shmoose-es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_AU" sourcelanguage="en_AU">
+<TS version="2.1" language="es" sourcelanguage="es">
 <context>
     <name>AboutPage</name>
     <message>
