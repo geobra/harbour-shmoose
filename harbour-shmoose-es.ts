eb7123fa5ee2e30cb6267e8f8e55eed5b1d62867
Carlos   Gonzalez <carlosgonz@protonmail.com>
2017-09-04 20:51:29 -0400

    Create harbour-shmoose-es.ts

---
+++ harbour-shmoose-es.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="es">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="13"/>
+        <source>Shmoose - Xmpp Client</source>
+        <translation>Shmoose - Cliente Xmpp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="33"/>
+        <source>Version </source>
+        <translation>Version</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="18"/>
+        <source>Contacts</source>
+        <translation>Contactos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="124"/>
+        <source>Join room by address</source>
+        <translation>Unirse a una sala por dirección</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="131"/>
+        <source>Add contact</source>
+        <translation>Agregar contactos</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationsPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationsPage.qml" line="18"/>
+        <source>Conversations</source>
+        <translation>Conversaciones</translation>
+    </message>
+</context>
+<context>
+    <name>CreateContactDialog</name>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="13"/>
+        <source>Add Contact</source>
+        <translation>Agregar un contacto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="20"/>
+        <source>Jid</source>
+        <translation>Jid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="27"/>
+        <source>NickName</source>
+        <translation>Alias</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePickerPage</name>
+    <message>
+        <location filename="../qml/pages/ImagePickerPage.qml" line="42"/>
+        <source>Select picture</source>
+        <translation>Seleccionar una foto</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoomDialog</name>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="13"/>
+        <source>Join Room by address</source>
+        <translation>Unirse a una sala por dirección</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="20"/>
+        <source>Room address</source>
+        <translation>dirección de sala</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="27"/>
+        <source>Room name</source>
+        <translation>Nombre de la sala</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="16"/>
+        <source>Login</source>
+        <translation>Iniciar sesión</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="21"/>
+        <source>Login to Jabber Server</source>
+        <translation>Iniciar sesión en Jabber</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="28"/>
+        <source>jid@server.com</source>
+        <translation>jid@server.com</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="29"/>
+        <source>Jid</source>
+        <translation>Jid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="40"/>
+        <source>password</source>
+        <translation>contraseña</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="42"/>
+        <source>Password</source>
+        <translation>Contraseña</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="61"/>
+        <source>Save credentials (unencrypted)</source>
+        <translation>Guardar credenciales (sin encriptar)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="71"/>
+        <source>Connect</source>
+        <translation>Conectar</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="60"/>
+        <source>Contacts</source>
+        <translation>Contactos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="61"/>
+        <source>Conversations</source>
+        <translation>Conversaciones</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="62"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="239"/>
+        <source>Enter message...</source>
+        <translation>Escribir mensaje...</translation>
+    </message>
+</context>
+</TS>
