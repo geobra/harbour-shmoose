062143c0f65d9736ef06d48af508d30f1557b71a
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-12-12 22:04:11 +0100

    Create sv_SE.ts
    Here's Swedish translation if wanted. Thanks for the app!

---
+++ resources/translations/sv_SE.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE" sourcelanguage="en_AU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="13"/>
+        <source>Shmoose - Xmpp Client</source>
+        <translation>Shmoose - XMPP-client</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="33"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="18"/>
+        <source>Contacts</source>
+        <translation>Kontakter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="124"/>
+        <source>Join room by address</source>
+        <translation>Anslut till rum via adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="131"/>
+        <source>Add contact</source>
+        <translation>Lägg till kontakt</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationsPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationsPage.qml" line="18"/>
+        <source>Conversations</source>
+        <translation>Konversationer</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>online</source>
+        <translation>online</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>offline</source>
+        <translation>offline</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="46"/>
+        <source>connected</source>
+        <translation>ansluten</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="50"/>
+        <source>disconnected</source>
+        <translation>inte ansluten</translation>
+    </message>
+</context>
+<context>
+    <name>CreateContactDialog</name>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="13"/>
+        <source>Add Contact</source>
+        <translation>Lägg till kontakt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="20"/>
+        <source>Jid</source>
+        <translation>Jid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="27"/>
+        <source>NickName</source>
+        <translation>Smeknamn</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePickerPage</name>
+    <message>
+        <location filename="../qml/pages/ImagePickerPage.qml" line="42"/>
+        <source>Select picture</source>
+        <translation>Välj bild</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoomDialog</name>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="13"/>
+        <source>Join Room by address</source>
+        <translation>Anslut till rum via adress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="20"/>
+        <source>Room address</source>
+        <translation>Rumsadress</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="27"/>
+        <source>Room name</source>
+        <translation>Rumsnamn</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>Logga in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="16"/>
+        <source>Welcome to Shmoose</source>
+        <translation>Välkommen till Shmoose</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="21"/>
+        <source>Login to Jabber Server</source>
+        <translation>Logga in till en Jabber-server</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="28"/>
+        <source>jid@server.com</source>
+        <translation>jid@server.com</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="29"/>
+        <source>Jid</source>
+        <translation>Jabber-ID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="40"/>
+        <source>password</source>
+        <translation>lösenord</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="42"/>
+        <source>Password</source>
+        <translation>Lösenord</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="61"/>
+        <source>Save credentials (unencrypted)</source>
+        <translation>Spara inloggningsuppgifter (okrypterat)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="71"/>
+        <location filename="../qml/pages/LoginPage.qml" line="91"/>
+        <source>Connect</source>
+        <translation>Anslut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="76"/>
+        <source>Connecting...</source>
+        <translation>Ansluter...</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="60"/>
+        <source>Contacts</source>
+        <translation>Kontakter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="61"/>
+        <source>Conversations</source>
+        <translation>Konversationer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="62"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="225"/>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="253"/>
+        <source>Enter message...</source>
+        <translation>Skriv meddelande...</translation>
+    </message>
+</context>
+</TS>
