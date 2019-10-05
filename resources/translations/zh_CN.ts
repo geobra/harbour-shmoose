5760a52aea26ef973b15596b25ded4abcb4b105f
ReleaseRoll <55141950+ReleaseRoll@users.noreply.github.com>
2019-10-05 15:56:47 +0800

    Create zh_CN.ts (#50)

---
+++ resources/translations/zh_CN.ts
@@ -0,0 +1,288 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_AU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="14"/>
+        <source>Shmoose - Xmpp Client</source>
+        <translation>Shmoose - Xmpp C客户端</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="27"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="34"/>
+        <source>Contributions:</source>
+        <translation>贡献:</translation>
+    </message>
+</context>
+<context>
+    <name>AttachmentPathsPage</name>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="12"/>
+        <source>Add Path</source>
+        <translation>添加路径</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="23"/>
+        <source>Attachment Search Paths</source>
+        <translation>附件搜索路径</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="37"/>
+        <source>Deleting</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="42"/>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="18"/>
+        <source>Contacts</source>
+        <translation>联系人</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="124"/>
+        <source>Join room by address</source>
+        <translation>通过地址加入房间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="131"/>
+        <source>Add contact</source>
+        <translation>添加联系人</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationsPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationsPage.qml" line="18"/>
+        <source>Conversations</source>
+        <translation>对话</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>online</source>
+        <translation>在线</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>offline</source>
+        <translation>离线</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="48"/>
+        <source>connected</source>
+        <translation>已连接</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="51"/>
+        <source>disconnected</source>
+        <translation>已断开</translation>
+    </message>
+</context>
+<context>
+    <name>CreateContactDialog</name>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="13"/>
+        <source>Add Contact</source>
+        <translation>添加联系人</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="20"/>
+        <source>JID</source>
+        <translation>JID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>名字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="27"/>
+        <source>NickName</source>
+        <translation>昵称</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePickerPage</name>
+    <message>
+        <location filename="../qml/pages/ImagePickerPage.qml" line="42"/>
+        <source>Select picture</source>
+        <translation>选取图片</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoomDialog</name>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="13"/>
+        <source>Join Room by address</source>
+        <translation>通过地址加入房间</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domain.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="20"/>
+        <source>Room address</source>
+        <translation>房间地址</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="27"/>
+        <source>Room name</source>
+        <translation>房间名称</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="22"/>
+        <source>Welcome to Shmoose</source>
+        <translation>欢迎使用 Shmoose</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="29"/>
+        <source>Login to Jabber Server</source>
+        <translation>登录到 Jabber 服务器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="36"/>
+        <source>jid@server.com</source>
+        <translation>jid@server.com</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="37"/>
+        <source>JID</source>
+        <translation>JID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="48"/>
+        <source>password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="50"/>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="71"/>
+        <source>Save credentials (unencrypted)</source>
+        <translation>保存登录凭证 (未加密)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="81"/>
+        <location filename="../qml/pages/LoginPage.qml" line="103"/>
+        <source>Connect</source>
+        <translation>连接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="86"/>
+        <source>Connecting...</source>
+        <translation>正在连接...</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="60"/>
+        <source>Contacts</source>
+        <translation>联系人</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="61"/>
+        <source>Conversations</source>
+        <translation>对话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="62"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="63"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+</context>
+<context>
+    <name>MessageStatusPage</name>
+    <message>
+        <location filename="../qml/pages/MessageStatusPage.qml" line="18"/>
+        <source>Message Status</source>
+        <translation>消息状态</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="225"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="230"/>
+        <source>Status</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="287"/>
+        <source>Enter message...</source>
+        <translation>正在输入消息...</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesPage</name>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="17"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="19"/>
+        <source>Privacy</source>
+        <translation>隐私</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="24"/>
+        <source>Send Read Notifications</source>
+        <translation>发送已读通知</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="30"/>
+        <source>Attachments</source>
+        <translation>附件</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="33"/>
+        <source>Edit attachment search paths</source>
+        <translation>编辑附近搜索路径</translation>
+    </message>
+</context>
+</TS>
