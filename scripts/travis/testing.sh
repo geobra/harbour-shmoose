a8371008625dcce0c0840c5e99ebbbd0f746a6eb
Georg <s.g.b@gmx.de>
2020-02-24 22:11:49 +0100

    some refactoring

--- scripts/travis/testing.sh
+++ scripts/travis/testing.sh
@@ -5,22 +5,6 @@ TESTPATH="testing"
 COVFILE="coverage.info"
 RESULTS="results"
 
-# a dbus session is needed
-if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
-	## if not found, launch a new one
-	eval `dbus-launch --sh-syntax`
-	echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
-fi
-
-echo "use $DBUS_SESSION_BUS_ADDRESS as dbus address"
-
-# build for testing
-mkdir $TESTPATH
-cd $TESTPATH
-qmake .. DEFINES+=TRAVIS DEFINES+=DBUS
-make
-
-
 GCOVRESULTSCOMMON="gcovresults"
 RESULTSC1=${TRAVIS_BUILD_DIR}/${RESULTS}/${GCOVRESULTSCOMMON}/c1/
 RESULTSC2=${TRAVIS_BUILD_DIR}/${RESULTS}/${GCOVRESULTSCOMMON}/c2/
@@ -32,6 +16,47 @@ mkdir -p $RESULTSC3
 
 BUILDTEST_PATH_DEPTH=$(echo "${TRAVIS_BUILD_DIR}/${TESTPATH}" | grep -o / | wc -l)
 echo "BPD ####### $BUILDTEST_PATH_DEPTH #############"
+
+collect_coverage_at_path_to_file()
+{
+	RPATH=$1
+	CFILE=$2
+	if [ "$(ls -A $RPATH)" ]; then
+		cp $RPATH/*.gcda ${TRAVIS_BUILD_DIR}/${TESTPATH}/
+		lcov --capture --directory ${TRAVIS_BUILD_DIR}/$TESTPATH --output-file ${TRAVIS_BUILD_DIR}/${RESULTS}/$CFILE
+		rm -f ${TRAVIS_BUILD_DIR}/$TESTPATH/*.gcda
+	fi
+
+	rm -f $RPATH/*.gcda
+}
+
+merge_client_coverage_to_file()
+{
+	TFILE=$1
+	APPEND=""
+	for CF in $(find ${TRAVIS_BUILD_DIR}/${RESULTS} -name "*.cov"); do
+		if [! -s $CF ]; then
+			APPEND="$APPEND -a $CF "
+		fi
+	done
+	lcov $APPEND -o  ${TRAVIS_BUILD_DIR}/$TFILE
+
+	find ${TRAVIS_BUILD_DIR}/${RESULTS} -type f -name "*.cov" -exec rm -f {} \;
+}
+
+# a dbus session is needed
+if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
+	## if not found, launch a new one
+	eval `dbus-launch --sh-syntax`
+	echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
+fi
+echo "use $DBUS_SESSION_BUS_ADDRESS as dbus address"
+
+# build for testing
+mkdir $TESTPATH
+cd $TESTPATH
+qmake .. DEFINES+=TRAVIS DEFINES+=DBUS
+make
 
 # build and run the roster test
 ${TRAVIS_BUILD_DIR}/scripts/travis/reset_ejabberd.sh
@@ -46,19 +71,12 @@ qmake .. && make
 xvfb-run -a -e /dev/stdout ./RosterTest
 
 # cp trace data to source dir; run lcov and generate test.cov
-cp $RESULTSC1/*.gcda ${TRAVIS_BUILD_DIR}/${TESTPATH}/
-lcov --capture --directory ${TRAVIS_BUILD_DIR}/$TESTPATH --output-file ${TRAVIS_BUILD_DIR}/t1c1.cov
-rm -f ${TRAVIS_BUILD_DIR}/$TESTPATH/*.gcda
+collect_coverage_at_path_to_file "$RESULTSC1" "c1.cov"
+collect_coverage_at_path_to_file "$RESULTSC2" "c2.cov"
+collect_coverage_at_path_to_file "$RESULTSC3" "c3.cov"
 
-cp $RESULTSC2/*.gcda ${TRAVIS_BUILD_DIR}/${TESTPATH}/
-lcov --capture --directory ${TRAVIS_BUILD_DIR}/$TESTPATH --output-file ${TRAVIS_BUILD_DIR}/t1c2.cov
-rm -f ${TRAVIS_BUILD_DIR}/$TESTPATH/*.gcda
-
-cp $RESULTSC2/*.gcda ${TRAVIS_BUILD_DIR}/${TESTPATH}/
-lcov --capture --directory ${TRAVIS_BUILD_DIR}/$TESTPATH --output-file ${TRAVIS_BUILD_DIR}/t1c3.cov
-rm -f ${TRAVIS_BUILD_DIR}/$TESTPATH/*.gcda
-
-lcov -a ${TRAVIS_BUILD_DIR}/t1c1.cov -a ${TRAVIS_BUILD_DIR}/t1c2.cov -a ${TRAVIS_BUILD_DIR}/t1c3.cov -o ${TRAVIS_BUILD_DIR}/${COVFILE}
+#lcov -a ${TRAVIS_BUILD_DIR}/c1.cov -a ${TRAVIS_BUILD_DIR}/c2.cov -a ${TRAVIS_BUILD_DIR}/c3.cov -o ${TRAVIS_BUILD_DIR}/${COVFILE}
+merge_client_coverage_to_file ${COVFILE}
 
 # build and run the plain 1to1 msg test
 #killall -9 harbour-shmoose
