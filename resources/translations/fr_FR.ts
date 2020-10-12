2938397ea0855795255498ae21c90bbd3d81a953
Tititesouris <quentin.brault.france@gmail.com>
2020-10-12 22:54:06 +0200

    French translation

---
+++ resources/translations/fr_FR.ts
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_AU" sourcelanguage="en_AU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="14"/>
+        <source>Shmoose - Xmpp Client</source>
+        <translation>Shmoose - Client Xmpp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="27"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="34"/>
+        <source>Contributions:</source>
+        <translation>Contributions :</translation>
+    </message>
+</context>
+<context>
+    <name>AttachmentPathsPage</name>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="12"/>
+        <source>Add Path</source>
+        <translation>Ajouter un chemin d'accès</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="23"/>
+        <source>Attachment Search Paths</source>
+        <translation>Emplacements de stockage des pièces jointes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="37"/>
+        <source>Deleting</source>
+        <translation>Suppression</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AttachmentPathsPage.qml" line="42"/>
+        <source>Remove</source>
+        <translation>Supprimer</translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <location filename="../../source/qml/ChatPage.qml" line="164"/>
+        <source>Enter message</source>
+        <translation>Tapez un message</translation>
+    </message>
+</context>
+<context>
+    <name>ContactsPage</name>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="18"/>
+        <source>Contacts</source>
+        <translation>Contacts</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="95"/>
+        <source>Remove</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="124"/>
+        <source>Join room by address</source>
+        <translation>Rejoindre un salon via une adresse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContactsPage.qml" line="131"/>
+        <source>Add contact</source>
+        <translation>Ajouter un contact</translation>
+    </message>
+</context>
+<context>
+    <name>ConversationsPage</name>
+    <message>
+        <location filename="../qml/pages/ConversationsPage.qml" line="18"/>
+        <source>Conversations</source>
+        <translation>Conversations</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>online</source>
+        <translation>en ligne</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="32"/>
+        <source>offline</source>
+        <translation>hors ligne</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="48"/>
+        <source>connected</source>
+        <translation>connecté</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="51"/>
+        <source>disconnected</source>
+        <translation>déconnecté</translation>
+    </message>
+</context>
+<context>
+    <name>CreateContactDialog</name>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="13"/>
+        <source>Add Contact</source>
+        <translation>Ajouter un contact</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domaine.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="20"/>
+        <source>JID</source>
+        <translation>JID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CreateContactDialog.qml" line="27"/>
+        <source>NickName</source>
+        <translation>Surnom</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePickerPage</name>
+    <message>
+        <location filename="../qml/pages/ImagePickerPage.qml" line="42"/>
+        <source>Select picture</source>
+        <translation>Choisir une image</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoomDialog</name>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="13"/>
+        <source>Join Room by address</source>
+        <translation>Rejoindre un salon via une adresse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="19"/>
+        <source>id@domain.org</source>
+        <translation>id@domaine.org</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="20"/>
+        <source>Room address</source>
+        <translation>Adresse du salon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="26"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JoinRoomDialog.qml" line="27"/>
+        <source>Room name</source>
+        <translation>Nom du salon</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Login</source>
+        <translation type="vanished">Connexion</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="22"/>
+        <source>Welcome to Shmoose</source>
+        <translation>Bienvenue chez Shmoose</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="29"/>
+        <source>Login to Jabber Server</source>
+        <translation>Connexion au serveur Jabber</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="36"/>
+        <source>jid@server.com</source>
+        <translation>jid@serveur.com</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="37"/>
+        <source>JID</source>
+        <translation>JID</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="48"/>
+        <source>password</source>
+        <translation>mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="50"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="71"/>
+        <source>Save credentials (unencrypted)</source>
+        <translation>Sauvegarder les identifiants (non chiffré)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="81"/>
+        <location filename="../qml/pages/LoginPage.qml" line="103"/>
+        <location filename="../../source/qml/LoginPage.qml" line="68"/>
+        <source>Connect</source>
+        <translation>Connexion</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoginPage.qml" line="86"/>
+        <source>Connecting...</source>
+        <translation>Connexion en cours...</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="60"/>
+        <source>Contacts</source>
+        <translation>Contacts</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="61"/>
+        <source>Conversations</source>
+        <translation>Conversations</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="62"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="63"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+<context>
+    <name>MessageStatusPage</name>
+    <message>
+        <location filename="../qml/pages/MessageStatusPage.qml" line="18"/>
+        <source>Message Status</source>
+        <translation>Statut du message</translation>
+    </message>
+</context>
+<context>
+    <name>MessagingPage</name>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="225"/>
+        <source>Copy</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="230"/>
+        <source>Status</source>
+        <translation>Statut</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MessagingPage.qml" line="287"/>
+        <source>Enter message...</source>
+        <translation>Tapez un message...</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesPage</name>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="17"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="19"/>
+        <source>Privacy</source>
+        <translation>Confidentialité</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="24"/>
+        <source>Send Read Notifications</source>
+        <translation>Envoyer les notifications de lecture</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="30"/>
+        <source>Attachments</source>
+        <translation>Pièces jointes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PreferencesPage.qml" line="33"/>
+        <source>Edit attachment search paths</source>
+        <translation>Modifier les emplacements de stockage des pièces jointes</translation>
+    </message>
+</context>
+</TS>
